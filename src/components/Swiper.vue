ca72b9623e51b1546ee9f4ba80fae2c3f5e7f879
FuzzyFade <1339184537@qq.com>
2018-12-23 18:29:58 +0800

    swiper ok

--- src/components/Swiper.vue
+++ src/components/Swiper.vue
@@ -1,21 +1,23 @@
 <template>
-    <swiper :options="swiperOption">
-        <!-- slides -->
-        <swiper-slide>
-            <div class="loopic" style="background-color: aquamarine"></div>
-        </swiper-slide>
-        <swiper-slide>
-            <div class="loopic" style="background-color: #ff8687"></div>
-        </swiper-slide>
-        <swiper-slide>
-            <div class="loopic" style="background-color: #ffdf8e"></div>
-        </swiper-slide>
-        <!-- Optional controls -->
-        <div class="swiper-pagination"  slot="pagination"></div>
-        <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
-        <!--<div class="swiper-button-next" slot="button-next"></div>-->
-        <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
-    </swiper>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide>
+                <div class="loopic" style="background-color: aquamarine"></div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="loopic" style="background-color: #ff8687"></div>
+            </swiper-slide>
+            <swiper-slide>
+                <div class="loopic" style="background-color: #ffdf8e"></div>
+            </swiper-slide>
+            <!-- Optional controls -->
+            <div class="swiper-pagination"  slot="pagination"></div>
+            <!--<div class="swiper-button-prev" slot="button-prev"></div>-->
+            <!--<div class="swiper-button-next" slot="button-next"></div>-->
+            <!--<div class="swiper-scrollbar"   slot="scrollbar"></div>-->
+        </swiper>
+        </div>
 </template>
 
 <script>
@@ -24,17 +26,30 @@
         data () {
             return {
                 swiperOption:{
-                    pageination:'.swiper-pagination',
-                    loop:true
+                    pagination : {
+                        el:'.swiper-pagination',
+                        type:'progressbar',
+                        // progressbarOpposite:true,
+                    },
+                    loop : true,
                 }
-
             }
         }
     }
 </script>
 
 <style lang="stylus" scoped>
-    .loopic
+    .wrapper >>> .swiper-pagination-progressbar
+        height 1rem
+        position absolute
+        top 8.75rem
+    .wrapper
+        overflow hidden
         width 100%
-        height 9rem
+        height 0
+        padding-bottom 9rem
+        background-color #ebf5f7
+        .loopic
+            width 100%
+            height 9rem
 </style>
