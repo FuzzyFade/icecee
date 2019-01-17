c8841002ad8a18238d0689f5b320454f8e339d0c
FuzzyFade <1339184537@qq.com>
2019-01-18 00:05:18 +0800

    bg 加了个toolbar

--- src/components/Motionimage.vue
+++ src/components/Motionimage.vue
@@ -1,8 +1,49 @@
 <template>
-    <div data-relative-input="true" ref="scene">
-        <div data-depth="0.2"> 我的第一个图层！</div>
-        <div data-depth="0.6"> 我的第二层！</div>
-    </div>
+    <v-app>
+        <v-toolbar
+            dark
+            clipped-left
+            height="60px"
+            color="#2e2e2e"
+        >
+            <v-btn icon>
+                <v-icon>arrow_back</v-icon>
+            </v-btn>
+            <v-toolbar-title class="title">
+                登录
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <div style="width: 100%;height: 100%">
+            <div data-relative-input="true" ref="scene">
+                <div data-depth="0.2">
+                    <v-flex xs12 sm6 offset-sm3>
+                        <v-card>
+                            <v-img
+                                    src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
+                                    aspect-ratio="2.75"
+                            ></v-img>
+
+                            <v-card-title primary-title>
+                                <div>
+                                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+                                </div>
+                            </v-card-title>
+
+                            <v-card-actions>
+                                <v-btn flat color="orange">Share</v-btn>
+                                <v-btn flat color="orange">Explore</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-flex>
+                </div>
+                <div data-depth="0.5">
+                    <v-btn color="green" dark round>Success</v-btn>
+                </div>
+            </div>
+        </div>
+    </v-app>
 </template>
 
 <script>
@@ -11,11 +52,14 @@
         name: "Motionimage",
 
         mounted() {
-            var para = new Parallax(this.$refs.scene);
+            var para = new Parallax(this.$refs.scene,{
+                pointerEvents : true
+            });
         }
     }
 </script>
 
-<style scoped>
-
+<style lang="stylus" scoped>
+    $color-pack = false
+    @import '~vuetify/src/stylus/main'
 </style>
